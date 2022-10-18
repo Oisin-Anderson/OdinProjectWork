31221ab48d96789935c4b78b84ea7e8b4e605775
A00260168A <99244146+Oisin-Anderson@users.noreply.github.com>
2022-10-18 15:15:59 +0100

    JavaScript Tests 3
    Done No. 5,6,7

--- Foundations/javascript-exercises-main/05_sumAll/sumAll.js
+++ Foundations/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let tot = 0
 
+    if(typeof min === 'string' || min instanceof String || min instanceof Array || typeof max === 'string' || max instanceof String || max instanceof Array){
+        return "ERROR"
+    }
+    else{
+        if( min >= 0 && max >= 0){
+            if(min > max){
+                let temp = min
+                min = max
+                max = temp
+            }
+
+            for(let i=min;i<=max;i++){
+                tot += i;
+            }
+        }
+        else{
+            return "ERROR"
+        }
+
+    }
+
+
+
+    return tot
 };
 
 // Do not edit below this line
