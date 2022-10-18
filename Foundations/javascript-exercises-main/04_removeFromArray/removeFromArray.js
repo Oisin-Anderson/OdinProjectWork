6dd037399e54ad7a64e1958759dd9a11953fdbd6
A00260168A <99244146+Oisin-Anderson@users.noreply.github.com>
2022-10-18 13:03:15 +0100

    JavaScript Tests 2
    No. 4 done

--- Foundations/javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ Foundations/javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(sarr, var1, var2, var3, var4) {
+    let farr = sarr
+    if(var1 != null){
+        farr = farr.filter(data => data != var1);
+    }
+    if(var2 != null){
+        farr = farr.filter(data => data != var2);
+    }
+    if(var3 != null){
+        farr = farr.filter(data => data != var3);
+    }
+    if(var4 != null){
+        farr = farr.filter(data => data != var4);
+    }
 
+    return farr
 };
 
 // Do not edit below this line
