c4bd9e243093c363325d18dbb0cbf6ba58168eb2
A00260168A <99244146+Oisin-Anderson@users.noreply.github.com>
2022-10-18 11:55:36 +0100

    JavaScript Tests 1
    Have 3 done

--- Foundations/javascript-exercises-main/03_reverseString/reverseString.js
+++ Foundations/javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(start) {
+    let split = start.split("")
+    let array = split.reverse()
+    let end = array.join("")
 
+    return end
 };
 
 // Do not edit below this line
