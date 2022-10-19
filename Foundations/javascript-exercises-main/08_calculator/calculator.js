023f801173f907978bd5a2d98e7653d84ad992cf
A00260168A <99244146+Oisin-Anderson@users.noreply.github.com>
2022-10-19 23:34:58 +0100

    JavaScripts Tests 4
    Test No.8 Done

--- Foundations/javascript-exercises-main/08_calculator/calculator.js
+++ Foundations/javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,61 @@
-const add = function() {
+const add = function(num1, num2) {
+  total = num1 + num2
+  return total
+};
+
+const subtract = function(num1, num2) {
+  total = num1 - num2
+  return total
+};
+
+const sum = function(arr) {
+
+  let len = arr.length
+  let total = 0
+  if(len == 0){
+    total = 0
+  }
+  else{
+    for(let i=0; i< len; i++){
+      total += arr[i]
+    }
+  }
+
+  return total
 	
 };
 
-const subtract = function() {
-	
+const multiply = function(arr) {
+  let len = arr.length
+  let total = arr[0]
+  for(let i=1; i< len; i++){
+    total = arr[i] * total
+  }
+
+  return total
 };
 
-const sum = function() {
-	
+const power = function(num, pow) {
+  let total = 0
+	total = Math.pow(num, pow)
+
+  return total
+  
 };
 
-const multiply = function() {
+const factorial = function(num) {
+  let total = num
 
-};
+  if(num <= 0){
+    total = 1
+  }
+  else{
+    for(let i = num - 1; i > 0; i--){
+      total = i * total
+    }
+  }
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+  return total
 };
 
 // Do not edit below this line
