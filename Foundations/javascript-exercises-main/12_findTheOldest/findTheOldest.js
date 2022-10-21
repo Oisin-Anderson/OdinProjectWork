ff30ab8048ddb6117e211c7e1b5aa3e9d3c498e4
A00260168A <99244146+Oisin-Anderson@users.noreply.github.com>
2022-10-21 22:04:50 +0100

    JavaScript Tests 6
    Done All

--- Foundations/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ Foundations/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let person = {}
+    let birth = 0
+    let death = 0
+    let age = 0
+    let max = 0
+    let oldest = {}
+
+    for(let i=0;i<people.length;i++){
+        person = people[i]
+        birth = person.yearOfBirth
+        if(person.yearOfDeath == null){
+            death = 2022
+        }
+        else{
+            death = person.yearOfDeath
+        }
+        age = death - birth
+
+        if(age > max){
+            max = age
+            oldest = person
+        }
+
+    }
+
+
+
+
+    return oldest
 };
 
 // Do not edit below this line
