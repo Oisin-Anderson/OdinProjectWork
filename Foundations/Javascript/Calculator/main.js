85b906f38950255bb3d474b37fcc22015fbf698d
Oisin Anderson <99244146+Oisin-Anderson@users.noreply.github.com>
2023-01-20 09:57:41 +0000

    Calculator
    Add,Sub,Mul, Div, Clear, Equals all work.
    Bimdas not implemented.
    Slight UI change.

--- Foundations/Javascript/Calculator/main.js
+++ Foundations/Javascript/Calculator/main.js
@@ -1,15 +1,90 @@
 $ = function(id){
 	return document.getElementById(id);
 }
+$$ = function(id){
+	return document.getElementsByClassName(id);
+}
+var numbers = [];
+var operators = ["blank"];
+var number = 0;
 
-function game(){
-    console.log("Hello");
+function nums(i){
+    var numerator = $$("number");
+    number += numerator[i].innerHTML;
+}
+function opps(i){
+    var operator = $$("operator");
+    var opp = operator[i].getAttribute('id');
+    if(number == 0){
+        numbers.push(parseInt($("result").innerHTML));
+    }
+    else{
+        numbers.push(parseInt(number));
+        console.log(numbers);
+        number = 0;
+    }
+    
+    if(opp == "equal"){
+        operators.push(opp);
+        calculation();
+        console.log("Calculation")
+    }
+    else if(opp == "clear"){
+        numbers = []
+        operators = ["blank"];
+        $("result").innerHTML = "0";
+    }
+    else{
+        operators.push(opp);
+
+    }
+    console.log(operators);
+
 }
 
+function calculation(){
+    var result = numbers[0];
+    for(let i=1;i<operators.length;i++){
+        if(operators[i] == "add"){
+            result += numbers[i];
+        }
+        else if(operators[i] == "sub"){
+            result -= numbers[i];
+            
+        }
+        else if(operators[i] == "mul"){
+            result = result * numbers[i];
+            
+        }
+        else if(operators[i] == "div"){
+            result = result/numbers[i];
+        }
+        else if(operators[i] == "blank"){
+
+        }
+        else{
+            $("result").innerHTML = result;
+            numbers = []
+            operators = ["blank"]
+        }
+    }
+}
 
 
 
 window.onload = function(){
-    document.getElementsByClassName("number").onclick = function(){game();}
-    
+    var numerator = $$("number");
+    for(let i=0;i<10;i++){
+        numerator[i].addEventListener("click", function(){
+            nums(i);
+        });
+    }
+    var operator = $$("operator");
+    for(let i=0;i<8;i++){
+        operator[i].addEventListener("click", function(){
+            opps(i);
+        });
+    }
+
 }
+    
