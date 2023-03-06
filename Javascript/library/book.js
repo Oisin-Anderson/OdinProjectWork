1617a768f214edca9f1ebbaf701ae526211a4260
Oisin Anderson <99244146+Oisin-Anderson@users.noreply.github.com>
2023-03-06 19:57:46 +0000

    Library
    Worked on Library Project.

--- Javascript/library/book.js
+++ Javascript/library/book.js
@@ -2,7 +2,7 @@ $ = function(id){
 	return document.getElementById(id);
 }
 
-let myLibrary = [];
+const library = [];
 
 function book(id, title, author, pages, read){
     this.id = id;
@@ -13,15 +13,53 @@ function book(id, title, author, pages, read){
 
 }
 
-function addBook(){
-    let num = myLibrary.length + 1;
-    //let formData = new FormData(document.querySelector('addBook'))
-    console.log(formData)
-    let title = $("title").innerHTML;
-    let author = $("author").innerHTML;
-    let pages = $("pages").innerHTML;
-    const entry = new book( num, title, author, pages, "Nope")
-    myLibrary.push(entry);
-    console.log("Hello");
-    console.log(myLibrary);
+
+function addBook(event){
+    const form = document.getElementById("addBook");
+    const formData = new FormData(form);
+    event.preventDefault();
+
+    const entry = new book();
+    entry.id = library.length + 1;
+    entry.title = formData.get("title");
+    entry.author = formData.get("author");
+    entry.pages = formData.get("pages");
+    entry.read = "No";
+    library.push(entry);
+    console.log(library)
+
+    refresh();
+    
+}
+
+function refresh(){
+    const output = $("books");
+    output.innerHTML = "";
+    for(let i=0; i<library.length; i++){
+        const div = document.createElement('div');
+        div.classList.add("card")
+        const title = document.createElement('h1');
+        title.textContent = library[i].title;
+        div.append(title);
+        const id = document.createElement('h4');
+        id.textContent = library[i].id;
+        div.append(id);
+        const author = document.createElement('h4');
+        author.textContent = library[i].author;
+        div.append(author);
+        const pages = document.createElement('h4');
+        pages.textContent = library[i].pages;
+        div.append(pages);
+        const read = document.createElement('h4');
+        read.textContent = library[i].read;
+        div.append(read);
+        console.log(div);
+        output.innerHTML += div.innerHTML;
+    }
+
+}
+
+window.onload = function(){
+    const form = document.getElementById("addBook");
+    form.addEventListener('submit', addBook);
 }
