b7a1f542ac2f1a00f05c67fe9cf9360a10cbad63
Oisin Anderson <99244146+Oisin-Anderson@users.noreply.github.com>
2023-03-07 15:23:51 +0000

    Library
    Library Project Complete for now.

--- Javascript/library/book.js
+++ Javascript/library/book.js
@@ -1,8 +1,13 @@
 $ = function(id){
 	return document.getElementById(id);
 }
+$$ = function(id){
+	return document.getElementsByClassName(id);
+}
 
-const library = [];
+let library = [];
+
+
 
 function book(id, title, author, pages, read){
     this.id = id;
@@ -20,11 +25,15 @@ function addBook(event){
     event.preventDefault();
 
     const entry = new book();
-    entry.id = library.length + 1;
+    if(library.length == 0){
+        entry.id = 1;
+    }else{
+        entry.id = parseInt(library[library.length - 1].id) + 1;
+    }
     entry.title = formData.get("title");
     entry.author = formData.get("author");
     entry.pages = formData.get("pages");
-    entry.read = "No";
+    entry.read = "Nope";
     library.push(entry);
     console.log(library)
 
@@ -37,29 +46,73 @@ function refresh(){
     output.innerHTML = "";
     for(let i=0; i<library.length; i++){
         const div = document.createElement('div');
-        div.classList.add("card")
+        //div.className = "card";
         const title = document.createElement('h1');
         title.textContent = library[i].title;
-        div.append(title);
+        div.appendChild(title);
         const id = document.createElement('h4');
         id.textContent = library[i].id;
-        div.append(id);
+        div.appendChild(id);
         const author = document.createElement('h4');
         author.textContent = library[i].author;
-        div.append(author);
+        div.appendChild(author);
         const pages = document.createElement('h4');
         pages.textContent = library[i].pages;
-        div.append(pages);
+        div.appendChild(pages);
         const read = document.createElement('h4');
         read.textContent = library[i].read;
-        div.append(read);
-        console.log(div);
-        output.innerHTML += div.innerHTML;
+        div.appendChild(read);
+        let del = document.createElement('button');
+        del.onclick = function(){removeBook(this.value)};
+        del.value = i;
+        del.innerHTML = "Delete";
+        div.appendChild(del);
+        let update = document.createElement('button');
+        update.onclick = function(){readBook(this.value)};
+        update.value = i;
+        update.innerHTML = "Change";
+        div.appendChild(update);
+        output.appendChild(div);
     }
+
+}
+
+function removeBook(i){
+    let trash = library.splice(i, 1);
+    refresh();
+
+}
+
+function readBook(i){
+    let temp = new book();
+    temp = library[i];
+    //console.log(temp);
+    if(temp.read == "Yeas"){
+        temp.read = "Nope"
+    }else if(temp.read == "Nope"){
+        temp.read = "Yeas"
+    }
+    library[i] = temp;
+    //console.log(temp)
+
+    refresh();
 
 }
 
 window.onload = function(){
     const form = document.getElementById("addBook");
     form.addEventListener('submit', addBook);
+    
+}
+
+function butCheck(){
+    console.log("Here");
+    let del = $$("removeBook");
+    for(let i=0; i<del.length; i++){
+        del[i].addEventListener("click", function(){removeBook(i)})
+    }
+    let update = $$("readBook");
+    for(let i=0; i<update.length; i++){
+        update[i].addEventListener("click", function(){updateBook(i)})
+    }
 }
