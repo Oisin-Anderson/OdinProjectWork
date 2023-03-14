3309491bc764376fcd70e0789bcf322b776e3d69
Oisin Anderson <99244146+Oisin-Anderson@users.noreply.github.com>
2023-03-14 12:49:24 +0000

    Restaurant
    Worked on Menu.js
    Almost done

--- Restuarant/webpack-demo/src/menu.js
+++ Restuarant/webpack-demo/src/menu.js
@@ -3,20 +3,74 @@ const menuLoad = () => {
     while(content.children.length > 1){
         content.children[1].remove();
     }
-    let homeimg = document.createElement('div');
-    let hometext = document.createElement('div');
-    let homeintro = document.createElement('div');
-    let homecopy = document.createElement('div');
-    homeimg.classList.add("homeimg");
-    homeintro.classList.add("homeintro");
-    hometext.classList.add("hometext");
-    homeintro.textContent = "Shorts";
-    homecopy.textContent = "Step back in time to explore this unique Fortification, visit our free exhibition, soak up the panoramic views that Shannonbridge Fort has to offer and take time to enjoy our Café & Gift Shop";
-    hometext.appendChild(homeintro);
-    hometext.appendChild(homecopy);
-    homeimg.appendChild(hometext);
+    let body = document.createElement('div');
+    body.classList.add("menubody");
 
-    content.appendChild(homeimg);
+    let header = document.createElement('h2');
+    header.textContent = "Food Menu";
+    body.appendChild(header);
+
+    let menu = document.createElement('div');
+    menu.classList.add("menu");
+    let items = ["tleft","tcenter","tright","bleft","bcenter","bright"]
+    let headers = ["Light Bites","Big Bites","Kiddie Bites","Sandwiches/Wraps","Pizza","Sips"]
+    
+    for(let i=0; i<items.length; i++){
+        let div = document.createElement('div');
+        div.classList.add(items[i]);
+        let title = document.createElement('h4');
+        title.textContent = headers[i];
+        title.classList.add("menutitle");
+        div.appendChild(title);
+
+        let options = document.createElement('div');
+        let food = [];
+        food = foods(items[i]);
+        for(let j=0;j<food.length;j++){
+            let item = document.createElement('h6');
+            item.textContent = food[j];
+            options.appendChild(item);
+
+        }
+        div.appendChild(options);
+        menu.appendChild(div);
+
+    }
+    body.appendChild(menu);
+
+
+    
+
+    content.appendChild(body);
 };
+
+function foods(option){
+    let tleft = ["Soup Of The Day","Fish Cakes","Caesar Salad","Goats Cheese Salad","The River Cafe Salad","Goujons","Smoked Salmon","Prawn Cocktail"];
+    let tcenter = ["Crispy Beer Battered Cod And Chips","Prime Hereford Beef Burger","Homemade Beef Lasagne","Vegetarian Option","Quiche Of The Day"];
+    let bright = ["Bottled Water","Tea","Coffee","Latte/Espresso/Cappuccino","Milk","Fruit Juice","Minerals","Wine: Red, White Or Rose","Bottled Beer"];
+    let bleft = ["Blt","Ham And Cheese Toastie","Cajun Chicken Wrap","Tuna And Sweetcorn Wrap","Veggie Wrap","Hot Chicken Wrap","Italian Wrap"];
+    let bcenter = ['12" Margherita','12" Pepperoni','12" Bacon And Mushroom','12" Goats Cheese And Red Onion','12" Ham And Pineapple','12" Cajun Chicken'];
+    let tright = ["Soup With Brown Bread",'8" Pizza Margherita',"Chicken Goujons And Chips","Sausages And Chips"];
+
+
+    if(option == "tleft"){
+        return tleft;
+    }
+    if(option == "tcenter"){
+        return tcenter;
+    }
+    if(option == "tright"){
+        return tright;
+    }
+    if(option == "bleft"){
+        return bleft;
+    }
+    if(option == "bcenter"){
+        return bcenter;
+    }
+    if(option == "bright"){
+        return bright;
+    }
+}
 
 export { menuLoad };
